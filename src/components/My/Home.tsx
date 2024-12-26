488b1d1e25738f709b02d01645b46a70354665a5
Tanvir <5takarcoin@gmail.com>
2024-12-27 03:08:18 +0600

    Start Landing

--- src/components/My/Home.tsx
+++ src/components/My/Home.tsx
@@ -1,10 +1,24 @@
-import { LoginCard } from "./LogIn";
-
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <LoginCard />
+    <div className="h-screen px-24">
+      {/* <LoginCard /> */}
       {/* <Calendar /> */}
+      <nav className="h-16 items-center flex justify-between">
+        <span>Fake</span>
+        <span>Nav Home About Logout</span>
+      </nav>
+      <div className="h-[500px] flex ">
+        <main className="bg-slate-50000 w-1/2 flex flex-col justify-center">
+          <h1 className="text-8xl font-bold mb-4 leading-[.8]">
+            <span>UNI</span> <span className="text-6xl">IS</span>
+            <div className="-mt-7 -mb-2">{"-------"}</div>
+            <span className="text-red-500">HARD</span>
+          </h1>
+          <p>Never forget your quizes</p>
+          <p>Never forget your deadlines</p>
+        </main>
+        <section className="w-1/2 bg-gray-600"></section>
+      </div>
     </div>
   );
 }
